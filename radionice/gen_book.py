da4d53184c56b7bda796c7eef5fdc6441d0653ed
Tomislav Mamić <tomislav.mamic1@gmail.com>
2019-06-25 17:52:07 +0200

    iskoristiv generator

--- radionice/gen_book.py
+++ radionice/gen_book.py
@@ -27,14 +27,17 @@ def gather_lesson_folders(top_folder):
 @why:   content table generation
 """
 __title = re.compile(r"\\dai{(.*?)}")
+__author = re.compile(r"\\author{(.*?)}")
 __id = re.compile(r"([0-9]{3})")
+__package = re.compile(r"(\\usepackage.*?$)", re.MULTILINE)
 def parse_lesson(file):
     data = {}
     with open(file, encoding = "utf-8") as f:
         content = f.read()
     data['title'] = re.findall(__title, content)[0]
     data['ID'] = int(re.findall(__id, file)[0])
-    data['path'] = file
+    data['authors'] = [x.strip() for x in re.findall(__author, content)[0].split(",")]
+    data['packages'] = re.findall(__package, content)
     return data
 
 """
@@ -44,20 +47,18 @@ def parse_lesson(file):
 """
 def get_lesson_data(top_folder):
     candidates = gather_lesson_folders(top_folder)
-    if top_folder[-1] not in ['\\', '/']:
-        top_folder += '/'
 
     # check if each dir contains a .tex with the same name
     lesson_files = []
     for c in candidates:
-        target = f"{top_folder}{c}/{c}.tex"
-        if os.path.isfile(target):
+        target = f"{c}/{c}.tex"
+        if os.path.isfile(top_folder + target):
             lesson_files.append(target)
         else:
             print(f"Folder '{c}' does not contain a .tex file with the same name!")
 
     # for each lesson, generate a dictionary with basic data
-    lessons = [parse_lesson(p) for p in lesson_files]
+    lessons = [{**parse_lesson(top_folder + p), **{'path': p}} for p in lesson_files]
     lessons = sorted(lessons, key = lambda p: p['ID'])
     return lessons
 
@@ -65,5 +66,61 @@ def get_lesson_data(top_folder):
 @what:  generate book .tex file
 @why:   that's our goal
 """
-def generate_book(top_folder):
+__banned_elements = ["\\author", "\\input", "!TeX", "{document}", "\\usepackage"]
+banned = lambda line: True if True in [x in line for x in __banned_elements] else False
+def generate_book(top_folder, title = "Skripta"):
+    # extend top folder with slash if needed
+    if top_folder[-1] not in ['\\', '/']:
+        top_folder += '/'
+
+    # parse lesson files
     lessons = get_lesson_data(top_folder)
+
+    # gather unique added packages and authors
+    unique_packages = []
+    unique_authors = {}
+    for l in lessons:
+        for a in l['authors']:
+            if a in unique_authors:
+                unique_authors[a] += 1
+            else:
+                unique_authors[a] = 1
+                
+        for p in l['packages']:
+            if p not in unique_packages:
+                unique_packages.append(p)
+    author = ", ".join(sorted(unique_authors))
+
+    # write book preamble
+    book = f"{__compiler_settings}\n"
+    book += "\\input{../../006_uvez.tex}\n"
+    # add missing packages
+    for p in unique_packages:
+        book += p + '\n'
+
+    # add book title and author data
+    book += f"\\title{{{title}}}\n"
+    book += f"\\author{{{author}}}\n"
+
+    # open book body
+    book += "\\begin{document}\n\n"
+    book += "{\\let\\cleardoublepage\clearpage\n\\maketitle\n\\tableofcontents}\n"
+
+    # merge fildered lesson text
+    first = True
+    for l in lessons:
+        book += f"\\newpage\n\\fakesection{{{l['title']}}}\n\n"
+        with open(top_folder + l['path'], 'r', encoding = "utf-8") as f:
+            for line in f.readlines():
+                if not banned(line):
+                    book += line
+
+    # close book body
+    book += "\\end{document}\n"
+
+    # print book to file
+    with open(top_folder + "book.tex", 'w', encoding = "utf-8") as f:
+        f.write(book)
+    return lessons
+
+d = generate_book("pocetni/lekcije")
