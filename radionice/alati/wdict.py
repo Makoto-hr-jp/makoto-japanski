94361ef2a0753ed3bf55caa47eb627dca95b1867
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-06-17 13:47:34 +0200

    Započeta skripta za generiranje pdf rječnika.

---
+++ radionice/alati/wdict.py
@@ -0,0 +1,14 @@
+"""
+@what:  Word dictionary parser - convert provisional text file format to word entry objects.
+@why:   Step in automated dictionary generation.
+@who:   Tomislav Mamić
+@when:  2022-06-17
+"""
+
+SOURCE = "./data/rijeci_pocetna.html"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
