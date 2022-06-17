271a389828b3dea985ad48fc4dc6531162b4174e
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-06-17 13:58:32 +0200

    Vađenje redaka tablice.

--- radionice/alati/wdict.py
+++ radionice/alati/wdict.py
@@ -5,7 +5,15 @@
 @when:  2022-06-17
 """
 
+# built-in
+import re
+
 SOURCE = "./data/rijeci_pocetna.html"
+TR_REGEX = re.compile(r"<tr>(.*?)</tr>", re.DOTALL)
+
+def extract_table_rows(path: str) -> "list[str]":
+    with open(path, encoding="utf-8") as src:
+        return re.findall(TR_REGEX, src.read())
 
 def main():
     pass
