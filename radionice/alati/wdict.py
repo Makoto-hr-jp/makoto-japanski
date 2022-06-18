a350cb34f9825689e6cf4564cf09644340563d6c
Tomislav Mamić <tomislav.mamic1@gmail.com>
2022-06-18 11:09:12 +0200

    Bolji opis retka i ispis grešaka.

--- radionice/alati/wdict.py
+++ radionice/alati/wdict.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup
 
 # built-in
 import re
+from dataclasses import dataclass
 
 SOURCE = "./data/rijeci_pocetna.html"
 TR_REGEX = re.compile(r"<tr>(.*?)</tr>", re.DOTALL)
@@ -20,6 +21,23 @@ FIELDS = [{'name': "lekcija", 'mandatory': False},
           {'name': "vrsta", 'mandatory': True},
           {'name': "značenje", 'mandatory': True},
           {'name': "napomena", 'mandatory': False}]
+
+@dataclass
+class Entry:
+    lekcija: str
+    riječ: str
+    čitanje: str
+    vrsta: str
+    značenje: str
+    napomena: str
+
+    def __repr__(self) -> str:
+        riječ = f"{self.riječ}" + (f" ({self.čitanje})" if self.čitanje else "")
+        objašnjenje = f"{self.značenje}" + (f" ({self.napomena})" if self.napomena else "")
+        return (f" lekcija: {self.lekcija}\n"
+                f"   riječ: {riječ} "
+                f"[{self.vrsta}]\n"
+                f"značenje: {objašnjenje}")
 
 def extract_table_rows(path: str) -> "list[str]":
     with open(path, encoding="utf-8") as src:
@@ -32,21 +50,36 @@ def extract_row_fields(row: str) -> "list[str]":
         ret.append(td.get_text())
     return ret
 
-def check_rows(rows: "list[str]") -> None:
+def check_rows(rows: "list[Entry]", log=None) -> None:
     for row in rows:
-        if len(row) != len(FIELDS):
-            print(f"Field number mismatch ({row}).")
-        for field, content in zip(FIELDS, row):
-            if field['mandatory'] and not content:
-                print(f"Empty mandatory field {field['name']} ({row}).")
+        for field in FIELDS:
+            if field['mandatory'] and not getattr(row, field['name']):
+                output = f"Empty mandatory field '{field['name']}' in\n{row}"
+                print(output)
+                if log:
+                    print(output, file=log)
 
 def main():
     rows = extract_table_rows(SOURCE)
-    rfields = []
-    for idx, row in enumerate(rows):
+    rfields = {}
+    cur_sect = ""
+    # assumptions
+    #   1. first entry starts with a section
+    #   2. sections are not fragmented
+    for row in rows:
         fields = extract_row_fields(row)
-        rfields.append(fields)
-    check_rows(rfields)
+        if fields[0]:
+            cur_sect = fields[0]
+            rfields[cur_sect] = []
+        else:
+            fields[0] = cur_sect
+        entry_data = Entry(*fields)
+        rfields[cur_sect].append(entry_data)
+
+    logname = ".".join(SOURCE.split(".")[:-1]) + ".log"
+    with open(logname, "w", encoding="utf-8") as log:
+        for sect, content in rfields.items():
+            check_rows(content, log)
 
 if __name__ == "__main__":
     main()
